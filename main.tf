631bab9bef58e32589f5da33029a45dbb366d973
Raghu K <raghuk.vit@gmail.com>
2026-07-19 21:36:23 +0530

    ec2-k9s TF Remove keypair need

--- main.tf
+++ main.tf
@@ -65,7 +65,6 @@ resource "aws_security_group" "all_open" {
 resource "aws_instance" "spot" {
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "r5.4xlarge"
-  key_name      = "devops"
 
   vpc_security_group_ids = [aws_security_group.ssh.id, aws_security_group.all_open.id]
 
